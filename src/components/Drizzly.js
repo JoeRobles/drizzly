63410528b1bc554e492e8b146bfc0e7a8e1ef469
Joe Robles <joe.robles@globant.com>
2017-09-18 17:59:08 -0500

    moved to drag anywhere approach

--- src/components/Drizzly.js
+++ src/components/Drizzly.js
@@ -1,29 +1,78 @@
-import React, {Component} from 'react';
-import {DragDropContext} from 'react-dnd';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import update from 'react/lib/update';
+import { DropTarget, DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-import Container from './Container';
+import Note from './Note';
 
-class Drizzly extends Component {
+const styles = {
+  width: '99%',
+  height: '700',
+  border: '1px solid black',
+  position: 'relative',
+};
+
+const noteTarget = {
+  drop(props, monitor, component) {
+    const item = monitor.getItem();
+    const delta = monitor.getDifferenceFromInitialOffset();
+    const left = Math.round(item.left + delta.x);
+    const top = Math.round(item.top + delta.y);
+
+    component.moveNote(item.id, left, top);
+  },
+};
+
+@DragDropContext(HTML5Backend)
+@DropTarget('note', noteTarget, connect => ({
+  connectDropTarget: connect.dropTarget(),
+}))
+
+export default class Drizzly extends Component {
+  static propTypes = {
+    connectDropTarget: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: {
+        a: { top: 20, left: 80, title: 'Drag me around' },
+        b: { top: 180, left: 20, title: 'Drag me too' },
+      },
+    };
+  }
+
+  moveNote(id, left, top) {
+    this.setState(update(this.state, {
+      notes: {
+        [id]: {
+          $merge: { left, top },
+        },
+      },
+    }));
+  }
 
   render() {
-    const style = {
-      display: "flex",
-      justifyContent: "space-around",
-      paddingTop: "20px"
-    };
+    const { connectDropTarget } = this.props;
+    const { notes } = this.state;
 
-    const list = [
-      {id: 1, text: "Item 1"},
-      {id: 2, text: "Item 2"},
-      {id: 3, text: "Item 3"}
-    ];
-
-    return (
-      <div style={{...style}}>
-        <Container id={1} list={list}/>
-      </div>
+    return connectDropTarget(
+      <div style={styles}>
+        {Object.keys(notes).map((key) => {
+          const { left, top, title } = notes[key];
+          return (
+            <Note
+              key={key}
+              id={key}
+              left={left}
+              top={top}
+            >
+              {title}
+            </Note>
+          );
+        })}
+      </div>,
     );
   }
 }
-
-export default DragDropContext(HTML5Backend)(Drizzly);
---
+++ src/components/Note.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { DragSource } from 'react-dnd';
+
+const style = {
+  position: 'absolute',
+  border: '1px solid gray',
+  backgroundColor: '#ff0',
+  padding: '0.5rem 1rem',
+  cursor: 'move',
+  height: '100px',
+  width: '100px',
+};
+
+const noteSource = {
+  beginDrag(props) {
+    const { id, left, top } = props;
+    return { id, left, top };
+  },
+};
+
+@DragSource('note', noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging(),
+}))
+
+class Note extends Component {
+  static propTypes = {
+    connectDragSource: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired,
+    id: PropTypes.any.isRequired,
+    left: PropTypes.number.isRequired,
+    top: PropTypes.number.isRequired,
+    children: PropTypes.node,
+  };
+
+  render() {
+    const { left, top, connectDragSource, isDragging, children } = this.props;
+    if (isDragging) {
+      return null;
+    }
+
+    return connectDragSource(
+      <div style={{ ...style, left, top }}>
+        {children}
+      </div>,
+    );
+  }
+}
+
+export default Note;
